ea79e1854b7e5040962338019efe1805ca48fdcf
Konrad Bartecki <konradbartecki@outlook.com>
2022-10-10 19:11:20 +0200

    Added support for print vs save to file on a server

--- socket_server.py
+++ socket_server.py
@@ -8,18 +8,15 @@ from cryptography.fernet import Fernet
 from crypt_service import CryptService
 
 print_data = True
+should_decrypt = False
 
 
 def server_program():
 
     host = socket.gethostname()
     port = 5000
-    should_decrypt = False
-
     server_socket = socket.socket()
-
     server_socket.bind((host, port))
-
     server_socket.listen(2)
     conn, address = server_socket.accept()
     print("Connection from: " + str(address))
@@ -34,31 +31,39 @@ def server_program():
         str_data = CryptService().decrypt(str_data)
         print("Decrypted into:")
         print(str_data)
-    if print_data:
-        # Using try/except to check if the data from the client
-        # is of a certain type i.e. binary, json, or xml. If the
-        # dta enters the try clause and does not have the correct
-        # data type, it moves onto the next try clause until it can be loaded
-        try:
-            # binary data
-            data_var = pickle.loads(str_data)
-            print(data_var)
-        except:
-            try:
-                # json data
-                data_json = json.loads(str_data)
-                print(data_json)
-            except:
-                try:
-                    # xml data
-                    print(data)
-                    tree = ET.parse(str_data)
-                    root = tree.getroot()
-                    print(root)
-                except:
-                    return
 
+    deserialized_data = deserialize_data(str_data)
+    if print_data:
+        print_data(deserialized_data)
+    else:
+        save_data(deserialized_data, "output_file.txt")
     conn.close()  # close the connection
+
+
+def deserialize_data(str_data):
+    # Using try/except to check if the data from the client
+    # is of a certain type i.e. binary, json, or xml. If the
+    # dta enters the try clause and does not have the correct
+    # data type, it moves onto the next try clause until it can be loaded
+    try:
+        # binary data
+        return  pickle.loads(str_data)
+    except:
+        try:
+            # json data
+            return json.loads(str_data)
+        except:
+            # xml data
+            tree = ET.parse(str_data)
+            return tree.getroot()
+
+
+def save_data(str_data):
+    open('output_file.txt', 'w').write(str_data)
+
+
+def print_data(str_data):
+    print(str_data)
 
 
 if __name__ == '__main__':
